82bbb12cd4d83472cf2c7dcb7a3494fca6a28655
Aden Fernandes <adenferns999@gmail.com>
2024-12-22 05:12:49 +0530

    Added redux store Added user slice Added auth methods for email and password Updated the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase" 
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg"
+import { useSelector } from "react-redux";
+import userFallBack from "../assets/user-logo-fallback.png"
 
 const Header = () => {
+  const navigate = useNavigate()
+  const user = useSelector(store => store.user);
+
+  const signOutHandler = () => {
+    signOut(auth).then(() => {
+      navigate("/")
+    }).catch((error) => {
+      navigate("/error");
+    });
+  }
+
   return (
-    <div className="absolute p-5 m-3 z-10">
-        <img src={logo} alt="Logo" className="w-44"/>
+    <div className="absolute p-5 z-10 w-screen flex justify-between">
+        <img src={logo} alt="Logo" className="w-40"/>
+
+        {user && (<div className="flex">
+          <img className="w-10 mr-2" src={userFallBack}  alt="User Logo"/>
+          <button onClick={signOutHandler}>Sign out</button>
+        </div>)}
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,22 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
 import { ValidateData } from "../utils/ValidateData";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import {auth} from "../utils/firebase"
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const[isLogin, setIsLogin] = useState(true);
   const[errorMessage, setErrorMessage] = useState(true);
 
+  const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const toggleSignIn = () => {
     setIsLogin(!isLogin);
@@ -26,28 +33,33 @@ const Login = () => {
       .then((userCredential) => {
         // Signed up 
         const user = userCredential.user;
-        console.log(user);
-        // ...
+        updateProfile(auth.currentUser, {
+          displayName: name.current.value, photoURL: "https://lh3.googleusercontent.com/a/ACg8ocI-a0ymdoDIxpUMPaBfcXHQ8tyiLxOg5-BIWkGeDUgCEp5yNFJF=s576-c-no"
+        }).then(() => {
+          console.log(auth);
+          const {uid, email, displayName, photoURL} = auth.currentUser;
+          dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}))
+          navigate("/browse")
+        }).catch((error) => {
+          setErrorMessage(error.messsage)
+        });
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         setErrorMessage(errorCode + "-" + errorMessage);
-        // ..
       });
     }else {
       signInWithEmailAndPassword(auth, email.current.value, password.current.value)
       .then((userCredential) => {
-        // Signed up 
+        // Sign In
         const user = userCredential.user;
-        console.log(user);
-        // ...
+        navigate("/browse");
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         setErrorMessage(errorCode + "-" + errorMessage);
-        // ..
       });
     }
   }
@@ -66,7 +78,7 @@ const Login = () => {
       <form onSubmit={(e) => e.preventDefault()} className="w-1/4 absolute p-8 px-10 bg-black bg-opacity-80 mx-auto my-40 right-0 left-0 rounded-md">
         <h2 className="text-white text-3xl font-semibold">{isLogin ? "Sign In" : "Sign Up"}
         </h2>
-        {!isLogin && <input type="text" className="p-2 my-5 w-full rounded-lg bg-gray-700 text-white" placeholder="Enter Full Name"/>}
+        {!isLogin && <input ref={name} type="text" className="p-2 my-5 w-full rounded-lg bg-gray-700 text-white" placeholder="Enter Full Name"/>}
         <input
           ref={email}
           type="email"
